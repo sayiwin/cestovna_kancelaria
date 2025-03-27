21ab60461469234f7953b672d5b9e41c39c889d8
Maksym Druziuk <druziukmaksym@gmail.com>
2025-03-27 12:17:24 +0100

    update 3
    Create login.php for registration page

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php 
+  include "./parts/header.php";
+  include_once "./parts/functions.php";
+?>
+<body>
+
+    <!-- Navigačný panel -->
+    <?php include_once "./parts/nav.php"; ?>
+
+    <!-- footer so základnými informáciami a kontaktami -->
+    <?php
+    include "./parts/footer.php";
+    ?>
+</body>
+</php>
