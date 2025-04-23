2ef40968cd75d8fafc70489149012393553a93fc
Maksym Druziuk <druziukmaksym@gmail.com>
2025-04-23 09:46:26 +0200

    Vytvorenie zväzku pomocou php. Pridanie súboru .json s údajmi. Vytvorenie karuselu pomocou php.
    Vytvorenie zväzku pomocou php. Pridanie súboru .json s údajmi. Vytvorenie karuselu pomocou php.

--- registracia.php
+++ registracia.php
@@ -8,78 +8,7 @@
   <?php include_once "./parts/nav_for_login.php"; ?>
 
   <!-- Karusel obrázkov s popismi -->
-  <div id="carouselExampleCaptions" class="carousel slide">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-    </div>
-    <div>
-    </div>
-    <div class="carousel-inner border border-dark border-3">
-      <div class="carousel-item active">
-        <img src="img/pexels-elina-sazonova-1850619.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption">
-          <div class="logo">
-            <h1>MaxCesta</h1>
-            <h5 class="text-decoration-underline">Maximálny oddych pre maximálnych osob</h5>
-          </div>
-          <div class="logotexton">
-            <h5>Dovoľte si dobrodružstvo, ktoré si zaslúžite!</h5>
-            <p>Predstavte si, ako sa slnko ponára do mora na tropickej pláži, alebo ako sa túlate po historických uličkách, ktoré rozprávajú príbehy dávnych civilizácií. MaxCesta vám ponúka cestu k nezabudnuteľným zážitkom, ktoré vám zmenia život!</p>
-            </div>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="img/pexels-davifnr-2064827.jpg" class="d-block w-100 imgs" alt="...">
-        <div class="carousel-caption">
-          <div class="logo">
-            <h1>MaxCesta</h1>
-            <h5 class="text-decoration-underline">Maximálny oddych pre maximálnych osob</h5>
-          </div>
-          <div class="logotexton">
-            <h5>Zažite slobodu a dobrodružstvo!</h5>
-            <p>Vypnite na chvíľu od každodenných starostí a ponorte sa do nových svetov! Prežite naplno každý moment, objavujte nové miesta, spoznávajte nové kultúry a ľudí. Cestovanie nie je len o destináciách, je to o tých nezabudnuteľných chvíľach, ktoré prežijete!</p>
-          </div>
-          </div>
-      </div>
-      <div class="carousel-item">
-        <img src="img/pexels-kristina-paul-713711153-27960501.jpg" class="d-block w-100 imgs" alt="...">
-        <div class="carousel-caption">
-          <div class="logo">
-            <h1>MaxCesta</h1>
-            <h5 class="text-decoration-underline">Maximálny oddych pre maximálnych osob</h5>
-          </div>
-          <div class="logotexton">
-            <h5>Pocítite v srdci túto slobodu, ktorá vás napĺňa!</h5>
-            <p>A čo je lepšie ako vidieť svet na vlastné oči? Či už relaxujete na pláži, potápate sa v krištáľovo čistých vodách alebo objavujete krásy miest, na ktoré nikdy nezabudnete - cesta je vždy viac než len cieľ.</p>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="img/pexels-mati-angulo-330211-930595.jpg" class="d-block w-100 imgs" alt="...">
-        <div class="carousel-caption">
-          <div class="logo">
-            <h1>MaxCesta</h1>
-            <h5 class="text-decoration-underline">Maximálny oddych pre maximálnych osob</h5>
-          </div>
-          <div class="logotexton">
-            <h5>Zaslúžite si to!</h5>
-            <p>Prečo čakať na dokonalý moment? Ten je práve teraz! Zarezervujte si svoju vysnívanú dovolenku a nechajte sa unášať vlnami nových zážitkov, ktoré vás čakajú. Znova sa nájdete a obnovíte svoju energiu. Podľa nás to rozhodne stojí za to!</p>
-          </div>
-          </div>
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
+  <?php render_carousel('home'); ?>
     
   <hr class="border border-dark border-3 opacity-75">
 
---
+++ tours/rodinny.php
@@ -0,0 +1,53 @@
+<li class="list-group-item">
+        <div class="mb-5">
+          <h4 class="logostartplus">Rodinný Tour:</h4>
+          <p>Rodinné túry sú ideálne pre rodiny s deťmi. Tieto túry zahŕňajú pohodlné trasy, ktoré sú prispôsobené pre rodinné cestovanie, návštevy atrakcií, múzeí a špeciálne programy pre deti. Ceny sa pohybujú od 100 do 210 € na osobu v závislosti od mesta a veľkosti skupiny, pričom rodiny s deťmi môžu získať výhodné zľavy.</p>
+          <div class="accordion border border-3 border-dark">
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed logotext" style="background-color: rgb(250, 247, 240);" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
+                  Rodinný Tour
+                </button>
+              </h2>
+              <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" style="background-color: rgb(250, 247, 240);">
+                <div class="accordion-body">
+                  <div class="tabulka">
+                    <table class="table table-striped">
+                      <tr>
+                        <th>Krajina:</th>
+                        <th>Pre 1 osobu:</th>
+                        <th>Pre 2 osoby:</th>
+                        <th>Pre rodinu:</th>
+                      </tr>
+                      <tr>
+                        <td>Paríž</td>
+                        <td>120 €</td>
+                        <td>230 €</td>
+                        <td>350 €</td>
+                      </tr>
+                      <tr>
+                        <td>Londýn</td>
+                        <td>110 €</td>
+                        <td>210 €</td>
+                        <td>330 €</td>
+                      </tr>
+                      <tr>
+                        <td>Berlín</td>
+                        <td>100 €</td>
+                        <td>190 €</td>
+                        <td>300 €</td>
+                      </tr>
+                      <tr>
+                        <td>Rím</td>
+                        <td>110 €</td>
+                        <td>210 €</td>
+                        <td>320 €</td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+</li>
